f456734ca2bce30cc763fb50d57a64b2fe203d50
Dante-Berth <bertinalexAI@protonmail.com>
2025-01-28 13:28:46 +0100

    @ pshysigym : modified rl folder structure

---
+++ rl/sac/launch_sac.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Number of instances to launch
+NUM_INSTANCES=8
+
+# Path to the Python file
+SCRIPT_PATH="rl/sac/sac.py"
+
+# Loop to launch instances
+for i in $(seq 1 $NUM_INSTANCES); do
+    echo "Launching instance $i with seed $i..."
+    nohup python "$SCRIPT_PATH" --seed "$i" > "output_$i.log" 2>&1 &
+    echo "Instance $i launched with PID $!"
+    sleep 2
+done
+
+echo "All $NUM_INSTANCES instances have been launched."
