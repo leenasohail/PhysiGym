9cce6eaf50ae053915e0e7329ce1ae5c0a3bb101
Dante-Berth <bertinalexAI@protonmail.com>
2025-04-28 09:25:22 +0200

    [UPDATE] replay buffers i added numba should be more efficient and it is tested

--- rl/utils/replay_buffer/smart_image_replay_buffer.py
+++ rl/utils/replay_buffer/smart_image_replay_buffer.py
@@ -3,6 +3,8 @@ import numpy as np
 import pickle
 import pandas as pd
 from tensordict import TensorDict
+from concurrent.futures import ThreadPoolExecutor
+from numba import njit, prange
 
 
 class ImgReplayBuffer(object):
@@ -198,3 +200,178 @@ class ImgReplayBuffer(object):
         ).astype(np.uint8)
 
         return grayscale_image[np.newaxis, :, :]  # Shape (1, height, width)
+
+
+# --- Add this function outside the class ---
+@njit(parallel=True)
+def colorize(x_valid, y_valid, types_valid, type_to_color_array, o_observation):
+    for i in prange(len(x_valid)):
+        type_idx = types_valid[i]
+        if 0 <= type_idx < type_to_color_array.shape[0]:
+            r, g, b = type_to_color_array[type_idx]
+        else:
+            r, g, b = 0, 0, 0
+
+        o_observation[0, x_valid[i], y_valid[i]] = r
+        o_observation[1, x_valid[i], y_valid[i]] = g
+        o_observation[2, x_valid[i], y_valid[i]] = b
+
+
+# --- Your class ---
+class MinimalImgReplayBuffer(object):
+    """
+    A lighter replay buffer that only stores (x, y, type_int) points per state,
+    and reconstructs images when sampling.
+    """
+
+    def __init__(
+        self,
+        action_dim: int,
+        device: torch.device,
+        buffer_size: int,
+        batch_size: int,
+        height: int,
+        width: int,
+        x_min: int,
+        y_min: int,
+        type_to_color: dict,
+        image_gray: bool,
+        num_workers: int = 4,
+    ):
+        self.device = device
+        self.buffer_size = int(buffer_size)
+
+        self.state = [None] * self.buffer_size
+        self.next_state = [None] * self.buffer_size
+        self.action = np.empty((self.buffer_size, action_dim), dtype=np.float32)
+        self.reward = np.empty((self.buffer_size, 1), dtype=np.float32)
+        self.done = np.empty((self.buffer_size, 1), dtype=np.uint8)
+
+        self.buffer_index = 0
+        self.full = False
+        self.batch_size = batch_size
+
+        self.height = height
+        self.width = width
+        self.x_min = x_min
+        self.y_min = y_min
+        self.image_gray = image_gray
+        self.num_workers = num_workers
+
+        # --- Precompute the type_to_color_array for fast lookup ---
+        self.type_to_color_array = self._make_type_to_color_array(type_to_color)
+
+    def _make_type_to_color_array(self, type_to_color):
+        max_type = max(type_to_color.keys())
+        color_array = np.zeros((max_type + 1, 3), dtype=np.uint8)
+        for t, color in type_to_color.items():
+            color_array[t] = np.array(color, dtype=np.uint8)
+        return color_array
+
+    def __len__(self):
+        return self.buffer_size if self.full else self.buffer_index
+
+    def add(self, df_cell, action, reward, next_df_cell, done, type_to_int):
+        state_array = self.extract_minimal_array(df_cell, type_to_int)
+        next_state_array = self.extract_minimal_array(next_df_cell, type_to_int)
+
+        self.state[self.buffer_index] = state_array
+        self.next_state[self.buffer_index] = next_state_array
+        self.action[self.buffer_index] = action
+        self.reward[self.buffer_index] = reward
+        self.done[self.buffer_index] = done
+
+        self.buffer_index = (self.buffer_index + 1) % self.buffer_size
+        self.full = self.full or self.buffer_index == 0
+
+    def extract_minimal_array(self, df_cell, type_to_int):
+        x = df_cell["x"].to_numpy()
+        y = df_cell["y"].to_numpy()
+        type_labels = df_cell["type"].map(type_to_int).to_numpy()
+
+        minimal_array = np.stack([x, y, type_labels], axis=1)
+        return minimal_array
+
+    def sample(self):
+        batch_size = self.batch_size
+        assert self.full or (self.buffer_index > batch_size), (
+            "Buffer does not have enough samples"
+        )
+
+        sample_index = np.random.randint(
+            0, self.buffer_size if self.full else self.buffer_index, batch_size
+        )
+
+        state_list = [self.state[i] for i in sample_index]
+        next_state_list = [self.next_state[i] for i in sample_index]
+
+        action = torch.as_tensor(self.action[sample_index], device=self.device)
+        reward = torch.as_tensor(self.reward[sample_index], device=self.device)
+        done = torch.as_tensor(self.done[sample_index], device=self.device)
+
+        with ThreadPoolExecutor(max_workers=self.num_workers) as executor:
+            if self.image_gray:
+                state_images = list(
+                    executor.map(self.minimal_array_to_grayscale, state_list)
+                )
+                next_state_images = list(
+                    executor.map(self.minimal_array_to_grayscale, next_state_list)
+                )
+            else:
+                state_images = list(
+                    executor.map(self.minimal_array_to_image, state_list)
+                )
+                next_state_images = list(
+                    executor.map(self.minimal_array_to_image, next_state_list)
+                )
+
+        state_tensor = torch.from_numpy(np.stack(state_images)).float().to(self.device)
+        next_state_tensor = (
+            torch.from_numpy(np.stack(next_state_images)).float().to(self.device)
+        )
+
+        sample = TensorDict(
+            {
+                "state": state_tensor,
+                "action": action,
+                "reward": reward,
+                "next_state": next_state_tensor,
+                "done": done,
+            },
+            batch_size=batch_size,
+            device=self.device,
+        )
+        return sample
+
+    def minimal_array_to_image(self, state_array):
+        x = state_array[:, 0]
+        y = state_array[:, 1]
+        type_int = state_array[:, 2].astype(int)
+
+        x_norm = (x - self.x_min).astype(int)
+        y_norm = (y - self.y_min).astype(int)
+
+        o_observation = np.zeros((3, self.height, self.width), dtype=np.uint8)
+
+        valid_mask = (
+            (0 <= x_norm)
+            & (x_norm < self.height)
+            & (0 <= y_norm)
+            & (0 <= y_norm < self.width)
+        )
+
+        x_valid = x_norm[valid_mask]
+        y_valid = y_norm[valid_mask]
+        types_valid = type_int[valid_mask]
+
+        # --- Fast colorization using Numba ---
+        colorize(x_valid, y_valid, types_valid, self.type_to_color_array, o_observation)
+
+        return o_observation
+
+    def minimal_array_to_grayscale(self, state_array):
+        color_image = self.minimal_array_to_image(state_array)
+        grayscale_image = np.dot(
+            color_image.transpose(1, 2, 0), [0.2989, 0.5870, 0.1140]
+        ).astype(np.uint8)
+        return grayscale_image[np.newaxis, :, :]
